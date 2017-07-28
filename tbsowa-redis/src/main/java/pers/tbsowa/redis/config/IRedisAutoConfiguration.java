94637712802a49a67bf1dacaa1367cddd7e4dd0b
garyxu <xutianbian@qq.com>
2017-07-29 00:21:20 +0800

    other commit

---
+++ tbsowa-redis/src/main/java/pers/tbsowa/redis/config/IRedisAutoConfiguration.java
@@ -0,0 +1,48 @@
+package pers.tbsowa.redis.config;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import pers.tbsowa.redis.utils.IRedisUtil;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class IRedisAutoConfiguration{
+	
+    private static Logger logger = Logger.getLogger(IRedisAutoConfiguration.class);  
+      
+    @Bean  
+    @ConfigurationProperties(prefix="spring.redis")  
+    public JedisPoolConfig getRedisConfig(){  
+        JedisPoolConfig config = new JedisPoolConfig();  
+        return config;  
+    }  
+      
+    @Bean  
+    @ConfigurationProperties(prefix="spring.redis")  
+    public JedisConnectionFactory getConnectionFactory(){  
+        JedisConnectionFactory factory = new JedisConnectionFactory();  
+        JedisPoolConfig config = getRedisConfig();  
+        factory.setPoolConfig(config);  
+        logger.info("JedisConnectionFactory bean init success.");  
+        return factory;  
+    }  
+      
+      
+    @Bean  
+    public RedisTemplate<?, ?> getRedisTemplate(){  
+        RedisTemplate<?,?> template = new StringRedisTemplate(getConnectionFactory());  
+        return template;  
+    }
+
+    @Bean
+    public IRedisUtil iRedisUtil(RedisTemplate<String, ?> redisTemplate){
+		return new IRedisUtil(redisTemplate);
+    }
+}
---
+++ tbsowa-redis/src/main/java/pers/tbsowa/redis/utils/IRedisUtil.java
@@ -0,0 +1,123 @@
+package pers.tbsowa.redis.utils;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import pers.tbsowa.root.utils.JSONUtils;
+
+public class IRedisUtil {
+	
+    private RedisTemplate<String, ?> redisTemplate;
+    
+	public IRedisUtil(RedisTemplate<String, ?> redisTemplate) {
+		this.redisTemplate = redisTemplate;
+	}
+
+	public boolean set(final String key, final String value) {
+		return redisTemplate.execute(new RedisCallback<Boolean>(){
+			@Override
+			public Boolean doInRedis(RedisConnection connection)
+					throws DataAccessException {
+				RedisSerializer<String> serializer = redisTemplate.getStringSerializer();
+				connection.set(serializer.serialize(key), serializer.serialize(value));
+				return true;
+			}
+		});
+	}
+
+	public String get(final String key) {
+		return redisTemplate.execute(new RedisCallback<String>(){
+			@Override
+			public String doInRedis(RedisConnection connection)
+					throws DataAccessException {
+				RedisSerializer<String> serializer = redisTemplate.getStringSerializer();
+				byte[] value = connection.get(serializer.serialize(key));
+				return serializer.deserialize(value);
+			}
+		});
+	}
+
+	/**
+	 * expire 为给定 key 设置过期时间
+	 * @param key
+	 * @param expire
+	 * @return
+	 */
+	public boolean expire(final String key, long expire) {
+		return redisTemplate.expire(key, expire, TimeUnit.SECONDS);
+	}
+
+	public <T> boolean setList(String key, List<T> list) {
+		String value = JSONUtils.toJson(list);
+		return set(key,value);
+	}
+
+	public <T> List<T> getList(String key, Class<T> clz) {
+		String json = get(key);
+		if(json!=null){
+			List<T> list = JSONUtils.toList(json, clz);
+			return list;
+		}
+		return null;
+	}
+
+	/**
+	 * 将一个或多个值插入到列表头部
+	 * @param key
+	 * @param obj
+	 * @return
+	 */
+	public long lpush(final String key, Object obj) {
+		final String value = JSONUtils.toJson(obj);
+		return redisTemplate.execute(new RedisCallback<Long>(){
+			@Override
+			public Long doInRedis(RedisConnection connection)
+					throws DataAccessException {
+				RedisSerializer<String>  serializer = redisTemplate.getStringSerializer();
+				return connection.lPush(serializer.serialize(key), serializer.serialize(value));
+			}
+		});
+	}
+
+	/**
+	 * 在列表中添加一个或多个值
+	 * @param key
+	 * @param obj
+	 * @return
+	 */
+	public long rpush(final String key, Object obj) {
+		final String value = JSONUtils.toJson(obj);
+		return redisTemplate.execute(new RedisCallback<Long>(){
+			@Override
+			public Long doInRedis(RedisConnection connection)
+					throws DataAccessException {
+				RedisSerializer<String>  serializer = redisTemplate.getStringSerializer();
+				return connection.rPush(serializer.serialize(key), serializer.serialize(value));
+			}
+		});
+	}
+
+	/**
+	 * 移出并获取列表的第一个元素
+	 * @param key
+	 * @return
+	 */
+	public String lpop(final String key) {
+		return redisTemplate.execute(new RedisCallback<String>(){
+			@Override
+			public String doInRedis(RedisConnection connection)
+					throws DataAccessException {
+				RedisSerializer<String>  serializer = redisTemplate.getStringSerializer();
+				byte[] value = connection.lPop(serializer.serialize(key));
+				return serializer.deserialize(value);
+			}
+		});
+	}
+
+}
